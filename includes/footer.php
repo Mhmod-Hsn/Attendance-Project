04438d63f58c84c2aa480efb70250beaa99c3a17
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-01-20 18:00:14 +0200

    toast library, update bootstrap, navbar done

--- includes/footer.php
+++ includes/footer.php
@@ -1,124 +1,64 @@
-<?php include "call-to-action.php"; ?>
-
-
-<?php include "footer-links.php"; ?>
-<?php include "copyrights.php"; ?>
-
-
-
-
-
-
-
-
 
 
 <script src="https://code.jquery.com/jquery-3.1.0.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+<!--<script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>-->
 
 
-<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!--<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>-->
 
 <script>
-    const isRTL = document.querySelector('html')?.attributes?.lang?.value === 'ar';
 
     $(document).ready(()=>{
 
-
-        // loader
+        // hide Loader
         document.querySelector('#loader').classList.add('hidden');
 
 
 
-	    $('.hero-slider').slick({
-	        rtl: isRTL,
-            autoplay: true,
-            infinite: true,
-		    responsive: [
-                {
-                    breakpoint: 700,
-                    settings: {
-                        arrows: false
-                    }
-                },
-		    ]
-	    });
 
-        $('#team-slider,#offers-slider').slick({
-            rtl: isRTL,
-            autoplay: true,
-            infinite: true,
-            arrows: false,
-            dots: true,
-            slidesToShow: 4,
-            swipeToSlide : true,
-            responsive: [
-                {
-                    breakpoint: 1300,
-                    settings: {
-                        slidesToShow: 4,
-                    }
-                },
-                {
-                    breakpoint: 1100,
-                    settings: {
-                        slidesToShow: 3,
-                    }
-                },
-                {
-                    breakpoint: 850,
-                    settings: {
-                        slidesToShow: 2,
-                    }
-                },
-                {
-                    breakpoint: 400,
-                    settings: {
-                        slidesToShow: 1,
-                    }
-                },
-            ]
+
+        // check if document is RTL or LTR
+        const isRTL = document.querySelector('html')?.attributes?.lang?.value === 'ar';
+
+
+
+        // Sweet Alert preparation
+        window.swal = swal.mixin({
+            confirmButtonColor: 'var(--secondary)',
+            cancelButtonColor: 'var(--primary)',
         });
+        window.toast = swal.mixin({
+
+            toast: true,
+            position: 'top',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 5000,
+            timerProgressBar: true,
+            onOpen: (toast) => {
+                toast.addEventListener('mouseenter', swal.stopTimer);
+                toast.addEventListener('mouseleave', swal.resumeTimer)
+            }
+        });
+
+
+
+
+
 
 
 
 
     });
 
-    // team members modal
-    $('#teamMemberModal').on('show.bs.modal', function (event) {
-        var e = $(event.relatedTarget) // Button that triggered the modal
-        // Extract info from data-* attributes
 
-	    let image= e.data('image')
-	    let name= e.data('name')
-	    let position= e.data('position')
-	    let nationality= e.data('nationality')
-	    let phone= e.data('phone')
-	    let whatsapp= e.data('whatsapp')
-        let email= e.data('email')
-
-        var modal = $(this)
-
-
-        $('#teamMemberModal .image')[0].src = image;
-        $('#teamMemberModal .name')[0].innerText = name;
-        $('#teamMemberModal .position')[0].innerText = position;
-        $('#teamMemberModal .nationality')[0].innerText = nationality;
-        $('#teamMemberModal .phone')[0].innerText = `${phone}`;
-        $('#teamMemberModal .phone')[0].href = `tel:${phone}`;
-        $('#teamMemberModal .whatsapp')[0].innerText = `${whatsapp}`;
-        $('#teamMemberModal .whatsapp')[0].href = `https://api.whatsapp.com/send/?phone=:${whatsapp}`;
-
-        $('#teamMemberModal .email')[0].innerText = email;
-        $('#teamMemberModal .email')[0].href = `mailto=:${email}`;
-
-
-    })
-
-
+/*
 
     // https://fancyapps.com/fancybox/3/docs/#options
     $('[data-fancybox="gallery"]').fancybox({
@@ -168,22 +108,16 @@
             }
         }
     });
+*/
 
-    ///////////////// fixed menu on scroll for desktop
-    if ($(window).width() > 992) {
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 250) {
-                $('nav').addClass("fixed-top");
-                // add padding top to show content behind navbar
-                $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
-            }else{
-                $('nav').removeClass("fixed-top");
-                // remove padding top from body
-                $('body').css('padding-top', '0');
-            }
-        });
-    } // end if
-
+    ///////////////// fixed menu on scroll
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 250) {
+            $('nav').addClass("fixed-top animate");
+        }else{
+            $('nav').removeClass("fixed-top animate");
+        }
+    });
 </script>
 
 </body>
--- index.php
+++ index.php
@@ -2,14 +2,6 @@
 <?php include('includes/header.php') ?>
 
 
-<?php include('includes/home/hero.php') ?>
-
-<?php include('includes/home/about.php') ?>
-<?php include('includes/home/services.php') ?>
-<?php include('includes/home/team.php') ?>
-<?php include('includes/home/what-we-offer.php') ?>
-<?php include('includes/home/offers.php') ?>
-
 
 
 <?php include('includes/footer.php') ?>
