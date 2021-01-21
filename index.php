9b55295ed5a0f43f785762052fa9b98270c9d90b
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-01-21 21:08:47 +0200

    hero, features done

--- index.php
+++ index.php
@@ -2,6 +2,87 @@
 <?php include('includes/header.php') ?>
 
 
+<div id="hero" class="py-lg-5 py-md-4 py-sm-3 py-2 ">
+    <div class="container">
+        <div class="row flex-md-row flex-column-reverse">
+            <div class="col-md-6 d-flex  justify-content-center  flex-column">
+                <h2 class="h1 text-justify text-primary mb-md-4 mb-sm-3 mt-md-3 mt-5">
+	                3 خطوات تفصلك عن انشاء متجرك الالكتروني
+                </h2>
+                <p class="lead text-justify text-gray">
+	                سجل، حدد تفاصيل متجرك، إدفع ثم انطلق لتضم المزيد من العملاء إلى متجرك لزيادة مبيعاتك وتحقيق المذيد من الربح من خلال منصة حلول للتجارة الإلكترونية
+                </p>
+	            <div class="btn-actions mt-md-4 mt-sm-2 text-center ">
+		            <a href="#" class="btn btn-primary mx-md-3 mx-2 my-2 px-4 rounded-pill min-width-btn shadow">تجربة المنصة</a>
+		            <a href="#" class="btn btn-secondary mx-md-3 mx-2 my-2 px-4 rounded-pill min-width-btn shadow">إبدأ الآن</a>
+
+	            </div>
+            </div>
+            <div class="col-md-6">
+                <img src="./images/hero-img.jpg" class="img-fluid my-4 my-md-2" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div id="features" class="py-lg-5 py-md-4 py-sm-3 py-2 text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="single-feature p-lg-5 p-md-4 p-3 mx-md-2 mx-5">
+					<img src="./images/feat1.jpg" class="img-fluid d-block mx-auto mb-3" alt="feat1">
+					<h6 class="font-weight-bold">متوافق مع جميع الأجهزة</h6>
+					<p class="font-weight-light small text-gray ">نقدم لك تطبيق خاص للمتجر الخاص بك يعمل على جميع الجوالات بالإضافة إلى الموقع الالكتروني الخاص بالمتجر الذي يعمل على جميع الأجهزة والمتصفحات</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="single-feature p-lg-5 p-md-4 p-3 mx-md-2 mx-5">
+					<img src="./images/feat2.jpg" class="img-fluid d-block mx-auto mb-3" alt="feat2">
+					<h6 class="font-weight-bold">تصاميم جذابة خاصة بمتجرك</h6>
+					<p class="font-weight-light small text-gray ">يتم تصميم متجرك بإحترافية تصمم بعناية من قبل فريق متخصص بأحدث التقنيات المتاحة لتتجاوب مع جميع الأجهزة لتوفر أفضل تجربة شراء لعملائك</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="single-feature p-lg-5 p-md-4 p-3 mx-md-2 mx-5">
+					<img src="./images/feat3.jpg" class="img-fluid d-block mx-auto mb-3" alt="feat3">
+					<h6 class="font-weight-bold">لوحة تحكم مميزة خاصة بالمتجر</h6>
+					<p class="font-weight-light small text-gray ">نصنع لمتجرك لوحة تحكم خاصة بك لتتماشى مع متطلباتك لتتمكن من إضافة وتعديل منتجاتك ومتابعة الطلبات والتحكم في كل تفاصيل المتجر بسهولة</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+<div id="why-us"class="py-lg-5 py-md-4 py-sm-3 py-2 ">
+	<div class="container">
+		<div class="offset-md-6 col-md-6 d-flex  justify-content-center  flex-column">
+			<h2 class="font-weight-bold">
+				انطلق الآن وانشر منتجاتك للعالم
+			</h2>
+			<h3 class="text-primary font-weight-bold">
+				عبر منصة حلول للتجارة الإلكترونية
+			</h3>
+			<p class="text-gray">بات الكثير من الأفراد في غالبية دول العالم يتجهون إلى المتاجر الإلكترونية من أجل الحصول على متطلباتهم الأساسية والترفيهية على حد سواء. مما جعلها تتميز بأهمية كبيرة في عالم التسوق، حيث تكمن  أهميته بما يعود فيه من فوائد وإيجابيات على كل طرف من الطرف البائع والطرف المشتري.</p>
+
+			<ul class="px-3 text-gray">
+				<li class="mb-2">توفير الوقت والجهد المبذولين في عملية التنقل من محل لآخر، حيث لا يتطلب الأمر سوى تصفح بعض المواقع المضمونة واختيار المناسب منها</li>
+				<li class="mb-2">إمكانية الحصول على معلومات تفصيلية حول المنتج، والقدرة على معرفة آراء الأشخاص فيه من خلال ميزة تقييم المنتج التي باتت متوفرة في غالبية المنصات الإلكترونية</li>
+				<li class="mb-2">إمكانية الحصول على عروض وخصومات جيدة، ناهيك عن الأسعار المنافسة، حيث تتميز هذه المتاجر بما توفره من تخفيضات دائمة وهدايا لتبقى ضمن المنافسة مع المحلات التقليدية وتتفوق عليها.</li>
+			</ul>
+
+			<div class="text-right">
+				<a href="#" class="btn btn-primary mx-md-3 mx-2 my-2 px-4 rounded-pill min-width-btn shadow">عرض المميزات</a>
+			</div>
+		</div>
+	</div>
+</div>
+
 
 
 <?php include('includes/footer.php') ?>
